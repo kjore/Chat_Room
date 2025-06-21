5fcfb1ef0c14b2b31306c14ad540f6cb79843612
kjore <zhangjingye@bupt.edu.cn>
2025-06-21 15:14:48 +0800

    群成员列表

--- src/GroupListCellRenderer.java
+++ src/GroupListCellRenderer.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
+
 public class GroupListCellRenderer extends DefaultListCellRenderer {
     @Override
     public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
